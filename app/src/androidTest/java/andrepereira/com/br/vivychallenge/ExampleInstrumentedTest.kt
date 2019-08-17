7d10d6ec957f3893f64738cd800102b75d1dc2ae
renanzalheiros <andrericardo.pereiraa@gmail.com>
2019-08-17 10:25:32 -0300

    Initial commit

---
+++ app/src/androidTest/java/andrepereira/com/br/vivychallenge/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package andrepereira.com.br.vivychallenge
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("andrepereira.com.br.vivychallenge", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/andrepereira/com/br/vivychallenge/MainActivity.kt
@@ -0,0 +1,12 @@
+package andrepereira.com.br.vivychallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

9ed3a686eb5c5a6374345f3962bd321168828836
renanzalheiros <andrericardo.pereiraa@gmail.com>
2019-08-18 15:28:15 -0300

    navigating to doctor list activity

--- app/src/main/java/andrepereira/com/br/vivychallenge/usecases/login/LoginActivityBinding.kt
+++ app/src/main/java/andrepereira/com/br/vivychallenge/usecases/login/LoginActivityBinding.kt
@@ -2,6 +2,7 @@ package andrepereira.com.br.vivychallenge.usecases.login
 
 import andrepereira.com.br.vivychallenge.MainActivity
 import android.content.Intent
+import android.view.View
 import android.widget.Button
 import androidx.databinding.BindingAdapter
 
@@ -9,5 +10,13 @@ import androidx.databinding.BindingAdapter
 fun clickLogin(view: Button, viewModel: LoginActivityViewModel) {
     view.setOnClickListener {
         viewModel.executeLogin()
+    }
+}
+
+@BindingAdapter("navigateToDoctorList")
+fun navigateToDoctorList(view: View, authStatus: AuthStatus) {
+    if (authStatus is AuthStatus.Authenticated) {
+        val intent = Intent(view.context, MainActivity::class.java)
+        view.context.startActivity(intent)
     }
 }
